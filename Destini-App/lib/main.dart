a635dfd0f866c7a309a2aa6801489dfcbf107735
Liron Abutbul <110838700+ladunjexa@users.noreply.github.com>
2023-06-15 20:35:15 +0300

    Add files via upload

---
+++ Destini-App/lib/main.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'story_brain.dart';
+
+StoryBrain storyBrain = StoryBrain();
+
+void main() {
+  runApp(const Destini());
+}
+
+class Destini extends StatelessWidget {
+  const Destini({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: const StoryPage(),
+    );
+  }
+}
+
+class StoryPage extends StatefulWidget {
+  const StoryPage({Key? key}) : super(key: key);
+
+  @override
+  State<StoryPage> createState() => _StoryPageState();
+}
+
+class _StoryPageState extends State<StoryPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/background.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        padding: const EdgeInsets.symmetric(vertical: 50.0, horizontal: 15.0),
+        constraints: const BoxConstraints.expand(),
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Expanded(
+                flex: 12,
+                child: Center(
+                  child: Text(
+                    storyBrain.getStory(),
+                    style: const TextStyle(
+                        fontSize: 25.0
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: TextButton(
+                  style: TextButton.styleFrom(backgroundColor: Colors.cyanAccent),
+                  onPressed: () {
+                    setState(() {
+                      storyBrain.nextStory(1);
+                    });
+                  },
+                  child: Text(
+                    storyBrain.getChoice1(),
+                    style: TextStyle(
+                      letterSpacing: 0.8,
+                      color: Colors.cyan.shade800,
+                      fontSize: 20.0
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              Expanded(
+                flex: 2,
+                child: Visibility(
+                  visible: storyBrain.buttonShouldBeVisible(),
+                  child: TextButton(
+                    style: TextButton.styleFrom(backgroundColor: Colors.cyanAccent),
+                    onPressed: () {
+                      setState(() {
+                        storyBrain.nextStory(2);
+                      });
+                    },
+                    child: Text(
+                      storyBrain.getChoice2(),
+                      style: TextStyle(
+                        letterSpacing: 0.8,
+                        color: Colors.cyan.shade800,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                )
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ Destini-App/readme.md
@@ -0,0 +1,3 @@
+![Banner](readme_assets/readme_banner.png)
+
+This repository houses an "Destini" App - Your Own Adventure Game built in Flutter 🔮
